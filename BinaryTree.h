9055a117f582b26eece1034d6e527a2a60287764
Manny820 <98365823+Manny820@users.noreply.github.com>
2022-12-23 23:22:05 -0800

    Create BinaryTree.h

---
+++ BinaryTree.h
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+//A node in the tree
+struct NodeType
+{
+  int info;
+  NodeType *leftLink;
+  NodeType *rightLink;
+};
+
+  //BinaryTree class
+class BinaryTreeType
+{
+  public:
+    bool isEmpty() const;
+    void inorderTraversal() const;
+    void preorderTraversal() const;
+    void postorderTraversal() const;
+    int treeHeight() const;
+    int treeNodeCount() const;
+    int treeLeavesCount() const;
+    int treeSwapTrees() const;
+    int treeSingleParent() const;
+    void destroyTree();
+    virtual bool search(const int &searchItem) const = 0;
+    virtual void insert(const int &insertItem) = 0;
+    virtual void deleteNode(const int &deleteItem) = 0;
+    BinaryTreeType(const BinaryTreeType &otherTree);
+    BinaryTreeType();
+    ~BinaryTreeType();
+  
+  protected:
+    NodeType  *root;
+  
+  private:
+    void copyTree(NodeType* &copiedTreeRoot,NodeType* otherTreeRoot);
+    void destroy(NodeType* &p);
+    void inorder(NodeType *p) const;
+    void preorder(NodeType *p) const;
+    void postorder(NodeType *p) const;
+    int height(NodeType *p) const;
+    int max(int x, int y) const;
+    int nodeCount(NodeType *p) const;
+    int leavesCount(NodeType *p) const;
+    int swapTrees(NodeType *p) const;
+    int singleParent(NodeType *p) const;
+  };
